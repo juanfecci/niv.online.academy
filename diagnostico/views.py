66200140621b59c523223edf96b3bdb95704ccb8
Juan Fecci <fecci.juan@gmail.com>
2025-01-09 18:07:07 -0300

    back base listo de la prueba de diagnostico

--- diagnostico/views.py
+++ diagnostico/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Question
 
 def prueba_diagnostico(request):
@@ -11,16 +11,21 @@ def prueba_diagnostico2(request):
     if request.method == 'POST':
         print("Entre al Post")
         questions_ids = request.POST.getlist('questions_ids')
-        selected_options = request.POST.getlist('selected_options')
+        #selected_options = request.POST.getlist('selected_options')
         selected_options_aux = request.POST.getlist('selected_options_74')
         print(questions_ids)
-        print(selected_options)
+        #print(selected_options)
         print(selected_options_aux)
 
-        for question_id, selected_option in zip(questions_ids, selected_options):
+        for question_id in questions_ids:
             print("Entre al for")
             question = Question.objects.get(id=question_id)
-            is_correct = int(selected_option) == question.correct_option
+            selected_option = request.POST.getlist('selected_options_' + question_id)
+
+            is_correct = int(selected_option[0]) == int(question.correct_option)
+            print(int(selected_option[0]))
+            print(question.correct_option)
+            print(is_correct)
 
             request.session['answers'].append({
                 'question_id': question_id,
@@ -40,19 +45,20 @@ def prueba_diagnostico2(request):
         request.session['page'] = 0
 
     questions_done = request.session['questions_done']
+    print(questions_done)
     remaining_questions = Question.objects.exclude(id__in=questions_done)
     actual_page = request.session['page'] + 1
 
     if not remaining_questions.exists():
-        return redirect('test_results')
+        answers = request.session.get('answers', [])
+        # Aquí puedes procesar las respuestas y calcular los resultados
+        # Por ejemplo, verificar respuestas correctas o calcular puntajes
+
+        return render(request, 'resultados.html', {'answers': answers})
 
     questions = list(remaining_questions)[:10]
 
+    print("Terminando")
     return render(request, 'diagnostico2.html', {'questions': questions, 'actual_page': actual_page})
 
-def test_results(request):
-    answers = request.session.get('answers', [])
-    # Aquí puedes procesar las respuestas y calcular los resultados
-    # Por ejemplo, verificar respuestas correctas o calcular puntajes
-
-    return render(request, 'resultados.html', {'answers': answers})
+    
